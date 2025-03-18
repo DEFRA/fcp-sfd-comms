cd4e27ba11af4abb4ed507675d395b55c6aa2e55
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2025-03-18 08:33:42 +0000

    Avoid shallow clone to fix Sonar

--- src/messaging/inbound/comms-request/handler.js
+++ src/messaging/inbound/comms-request/handler.js
@@ -9,30 +9,36 @@ import { parseSqsMessage } from '../../sqs/parse-message.js'
 
 const logger = createLogger()
 
+const handleMessage = async (sqsClient, message) => {
+  try {
+    const content = parseSqsMessage(message)
+
+    const processor = getCommsProcessor(content)
+
+    await processor(content)
+  } catch (err) {
+    logger.error(`Error processing message: ${err.message}`)
+    logger.info('Moving unprocessable message to dead letter queue')
+
+    await sendMessage(
+      sqsClient,
+      config.get('messaging.commsRequest.deadLetterUrl'),
+      message?.Body
+    )
+  }
+}
+
 const handleCommRequestMessages = async (sqsClient, messages) => {
   const completed = []
 
   for (const message of messages) {
     try {
-      const content = parseSqsMessage(message)
-
-      const processor = getCommsProcessor(content)
-
-      await processor(content)
-
-      completed.push(message)
+      await handleMessage(sqsClient, message)
     } catch (err) {
-      logger.error(`Error processing message: ${err.message}`)
-      logger.info('Moving unprocessable message to dead letter queue')
-
-      completed.push(message)
-
-      await sendMessage(
-        sqsClient,
-        config.get('messaging.commsRequest.deadLetterUrl'),
-        message?.Body
-      )
+      logger.error(`Error handling message error: ${err}`)
     }
+
+    completed.push(message)
   }
 
   return completed
--- src/messaging/sqs/send-message.js
+++ src/messaging/sqs/send-message.js
@@ -1,20 +1,12 @@
 import { SendMessageCommand } from '@aws-sdk/client-sqs'
 
-import { createLogger } from '../../logging/logger.js'
-
-const logger = createLogger()
-
 const sendMessage = async (sqsClient, queueUrl, message) => {
   const command = new SendMessageCommand({
     QueueUrl: queueUrl,
     MessageBody: message
   })
 
-  try {
-    await sqsClient.send(command)
-  } catch (err) {
-    logger.error(err)
-  }
+  await sqsClient.send(command)
 }
 
 export { sendMessage }
