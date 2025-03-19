bc9b02a86c58f7505b07e273c0f770678280894a
Rana Salem <ranatasalem@gmail.com>
2025-03-19 11:36:03 +0000

    logic for sending via notify

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,14 @@
 import convict from 'convict'
 
 import { serverConfig } from './server.js'
+import { notifyConfig } from './notify.js'
 import { awsConfig } from './aws.js'
 import { messagingConfig } from './messaging.js'
 import { mongoConfig } from './mongo.js'
 
 const config = convict({
   ...serverConfig,
+  ...notifyConfig,
   ...awsConfig,
   ...messagingConfig,
   ...mongoConfig
