b2b4351a9d43b9ae3319dd29a26233aebfaca30e
Rana Salem <ranatasalem@gmail.com>
2025-03-24 15:13:33 +0000

    add new constants: notify statuses

---
+++ src/constants/notify-statuses.js
@@ -0,0 +1,10 @@
+export const notifyStatuses = {
+  CREATED: 'created',
+  SENDING: 'sending',
+  DELIVERED: 'delivered',
+  PERMANENT_FAILURE: 'permanent-failure',
+  TEMPORARY_FAILURE: 'temporary-failure',
+  TECHNICAL_FAILURE: 'technical-failure',
+  INTERNAL_FAILURE: 'internal-failure',
+  VALIDATION_FAILURE: 'validation-failure'
+}
