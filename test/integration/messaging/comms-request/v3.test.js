e703fa8796204371e574d19024f170a4fa21eeaa
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2025-04-16 13:46:21 +0100

    Refactor integration tests

---
+++ test/integration/messaging/comms-request/v3.test.js
@@ -0,0 +1,141 @@
+import { afterAll, afterEach, beforeEach, describe, expect, test, vi } from 'vitest'
+
+import v3 from '../../../mocks/comms-request/v3.js'
+
+import { getAllEntities, clearCollection } from '../../../helpers/mongo.js'
+import { getMessages, getQueueSize, parseSqsMessage, resetQueue, sendMessage } from '../../../helpers/sqs.js'
+
+import notifyClient from '../../../../src/notify/notify-client.js'
+
+import { createLogger } from '../../../../src/logging/logger.js'
+import { startMessaging, stopMessaging } from '../../../../src/messaging/inbound/inbound.js'
+
+vi.mock('../../../../src/notify/notify-client.js', () => ({
+  default: {
+    sendEmail: vi.fn(),
+    getNotificationById: vi.fn()
+  }
+}))
+
+vi.mock('../../../../src/logging/logger.js', () => ({
+  createLogger: vi.fn().mockReturnValue({
+    info: vi.fn(),
+    warn: vi.fn(),
+    error: vi.fn()
+  })
+}))
+
+const commsRequestQueueUrl = process.env.COMMS_REQUEST_QUEUE_URL
+const commsRequestDlqUrl = process.env.COMMS_REQUEST_DEAD_LETTER_QUEUE_URL
+const dataIngestQueueUrl = process.env.DAL_INGEST_QUEUE_URL
+
+const mockLogger = createLogger('test')
+
+describe('v3 comms request processing integration', () => {
+  beforeEach(async () => {
+    vi.clearAllMocks()
+
+    await clearCollection('notificationRequests')
+
+    await resetQueue(commsRequestQueueUrl)
+    await resetQueue(commsRequestDlqUrl)
+    await resetQueue(dataIngestQueueUrl)
+
+    startMessaging()
+  })
+
+  test('should process a valid V3 comms request', async () => {
+    notifyClient.sendEmail.mockResolvedValue({
+      data: {
+        id: '79389915-7275-457a-b8ca-8bf206b2e67b'
+      }
+    })
+
+    notifyClient.getNotificationById.mockResolvedValue({
+      data: {
+        status: 'delivered'
+      }
+    })
+
+    const mockMessage = {
+      ...v3,
+      id: '15df79e7-806e-4c85-9372-a2e256a1d597',
+      data: {
+        ...v3.data,
+        commsAddresses: [
+          'test@example.com'
+        ]
+      }
+    }
+
+    await sendMessage(
+      commsRequestQueueUrl,
+      JSON.stringify(mockMessage)
+    )
+
+    await new Promise((resolve) => {
+      setTimeout(resolve, 5000)
+    })
+
+    const requests = await getAllEntities('notificationRequests', {
+      'message.id': mockMessage.id
+    })
+
+    expect(requests).toHaveLength(1)
+    expect(requests[0].recipients[0].status).toBe('delivered')
+    expect(requests[0].message).toEqual(mockMessage)
+
+    const eventCount = await getQueueSize(
+      dataIngestQueueUrl
+    )
+
+    expect(eventCount.available).toBe(2)
+
+    const events = await getMessages(
+      dataIngestQueueUrl
+    )
+
+    const parsedEvents = events.map((event) => parseSqsMessage(event))
+
+    expect(parsedEvents).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(String),
+          source: 'fcp-sfd-comms',
+          time: expect.any(String),
+          type: 'uk.gov.fcp.sfd.notification.received',
+          data: {
+            correlationId: '15df79e7-806e-4c85-9372-a2e256a1d597'
+          },
+          datacontenttype: 'application/json',
+          specversion: '1.0'
+        }),
+        expect.objectContaining({
+          id: expect.any(String),
+          source: 'fcp-sfd-comms',
+          time: expect.any(String),
+          type: 'uk.gov.fcp.sfd.notification.delivered',
+          data: {
+            correlationId: '15df79e7-806e-4c85-9372-a2e256a1d597',
+            commsAddresses: 'test@example.com',
+            statusDetails: {
+              status: 'delivered'
+            }
+          },
+          datacontenttype: 'application/json',
+          specversion: '1.0'
+        })
+      ])
+    )
+
+    expect(mockLogger.info).toHaveBeenCalledWith('Comms V3 request processed successfully, eventId: 15df79e7-806e-4c85-9372-a2e256a1d597')
+  })
+
+  afterEach(async () => {
+    stopMessaging()
+  })
+
+  afterAll(async () => {
+    await clearCollection('notificationRequests')
+  })
+}, 60000)
