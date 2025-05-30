0a39f7307ba66721ed8e81f1b76f8f49575253fc
Amirs Ismuhametovs <98330195+JungaBunga@users.noreply.github.com>
2025-05-30 15:26:07 +0100

    SNS to SQS wrapper handler (#25)
    #patch

--- src/messaging/sqs/parse-message.js
+++ src/messaging/sqs/parse-message.js
@@ -4,6 +4,10 @@ const parseSqsMessage = (message) => {
   try {
     const body = JSON.parse(message.Body)
 
+    if (body.Type === 'Notification' && body.TopicArn) {
+      return JSON.parse(body.Message)
+    }
+
     return body
   } catch (error) {
     throw new UnprocessableMessageError('Invalid message', {
