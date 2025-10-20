05f42c475560fcc44b04ed984a6d65a77f673faf
Rana Salem <ranatasalem@gmail.com>
2025-10-20 15:53:29 +0100

    update queue name: fcp_fdm -> fcp_fdm_events

--- compose/start-localstack.sh
+++ compose/start-localstack.sh
@@ -4,9 +4,9 @@ aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name fcp_sfd_c
 
 aws --endpoint-url=http://localhost:4566 sns create-topic --name fcp_sfd_comm_events
 
-aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name fcp_fdm-deadletter
-aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name fcp_fdm --attributes "{\"RedrivePolicy\": \"{\\\"deadLetterTargetArn\\\":\\\"arn:aws:sqs:eu-west-2:000000000000:fcp_fdm-deadletter\\\",\\\"maxReceiveCount\\\":\\\"10\\\"}\"}"
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name fcp_fdm_events-deadletter
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name fcp_fdm_events --attributes "{\"RedrivePolicy\": \"{\\\"deadLetterTargetArn\\\":\\\"arn:aws:sqs:eu-west-2:000000000000:fcp_fdm_events-deadletter\\\",\\\"maxReceiveCount\\\":\\\"10\\\"}\"}"
 
-aws --endpoint-url=http://localhost:4566 sqs set-queue-attributes --queue-url http://localhost:4566/000000000000/fcp_fdm --attributes "{\"Policy\":\"{\\\"Statement\\\":[{\\\"Effect\\\":\\\"Allow\\\",\\\"Principal\\\":{\\\"Service\\\":\\\"sns.amazonaws.com\\\"},\\\"Action\\\":\\\"sqs:SendMessage\\\",\\\"Resource\\\":\\\"arn:aws:sqs:eu-west-2:000000000000:fcp_fdm\\\",\\\"Condition\\\":{\\\"ArnEquals\\\":{\\\"aws:SourceArn\\\":\\\"arn:aws:sns:eu-west-2:000000000000:fcp_sfd_comm_events\\\"}}}]}\"}"
+aws --endpoint-url=http://localhost:4566 sqs set-queue-attributes --queue-url http://localhost:4566/000000000000/fcp_fdm_events --attributes "{\"Policy\":\"{\\\"Statement\\\":[{\\\"Effect\\\":\\\"Allow\\\",\\\"Principal\\\":{\\\"Service\\\":\\\"sns.amazonaws.com\\\"},\\\"Action\\\":\\\"sqs:SendMessage\\\",\\\"Resource\\\":\\\"arn:aws:sqs:eu-west-2:000000000000:fcp_fdm_events\\\",\\\"Condition\\\":{\\\"ArnEquals\\\":{\\\"aws:SourceArn\\\":\\\"arn:aws:sns:eu-west-2:000000000000:fcp_sfd_comm_events\\\"}}}]}\"}"
 
-aws --endpoint-url=http://localhost:4566 sns subscribe --topic-arn arn:aws:sns:eu-west-2:000000000000:fcp_sfd_comm_events --protocol sqs --notification-endpoint arn:aws:sqs:eu-west-2:000000000000:fcp_fdm
+aws --endpoint-url=http://localhost:4566 sns subscribe --topic-arn arn:aws:sns:eu-west-2:000000000000:fcp_sfd_comm_events --protocol sqs --notification-endpoint arn:aws:sqs:eu-west-2:000000000000:fcp_fdm_events
