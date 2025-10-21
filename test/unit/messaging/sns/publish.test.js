1ade64aa3e91d5731c27c435ac03f460d6d42a13
Rana Salem <127218837+rtasalem@users.noreply.github.com>
2025-10-21 16:14:31 +0100

    SFD2-424 Remove FiFo logic (#31)

--- test/unit/messaging/sns/publish.test.js
+++ test/unit/messaging/sns/publish.test.js
@@ -22,27 +22,7 @@ describe('SNS Publish', () => {
     vi.clearAllMocks()
   })
 
-  test('should receive and execute publish command if SNS topic is FiFo', async () => {
-    const topicArn = 'arn:aws:sns:eu-west-2:000000000000:fcp_fdm_events.fifo'
-
-    const message = {
-      test: 'hello world',
-      id: '149C5ACA-C971-45BA-8D94-9664A91B5471'
-    }
-
-    await publish(mockSnsClient, topicArn, message)
-
-    expect(PublishCommand).toHaveBeenCalledWith({
-      TopicArn: 'arn:aws:sns:eu-west-2:000000000000:fcp_fdm_events.fifo',
-      Message: JSON.stringify(message),
-      MessageGroupId: message.id,
-      MessageDeduplicationId: message.id
-    })
-
-    expect(mockSnsClient.send).toHaveBeenCalledTimes(1)
-  })
-
-  test('should receive and execute publish command if SNS topic is not FiFo', async () => {
+  test('should receive and execute publish command', async () => {
     const topicArn = 'arn:aws:sns:eu-west-2:000000000000:fcp_fdm_events'
 
     const message = {
