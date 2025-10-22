8c662e08048fead83e573cf162813b14b9730470
Rana Salem <ranatasalem@gmail.com>
2025-10-22 15:41:33 +0100

    undo changes to db client init

--- src/db/db-client.js
+++ src/db/db-client.js
@@ -1,6 +1,9 @@
 import { MongoClient } from 'mongodb'
+
 import { config } from '../config/index.js'
+
 import { createSecureContext } from '../api/common/helpers/secure-context/secure-context.js'
+
 import { createLogger } from '../logging/logger.js'
 
 const logger = createLogger()
