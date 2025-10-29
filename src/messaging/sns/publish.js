b6383602501c2a4c06e687580410098b00d0159e
Simon Dunn <simondunn90@gmail.com>
2025-10-29 10:50:37 +0000

    lint

--- src/messaging/sns/publish.js
+++ src/messaging/sns/publish.js
@@ -7,8 +7,7 @@ const publish = async (snsClient, topicArn, message) => {
     Message: JSON.stringify(message)
   }
 
-  
-  debugLog(message) 
+  debugLog(message)
   // this uses logger.debug which will only execute when cdp-app-config value LOG_LEVEL is set to debug
   // any messages sent to the debugLog function are sanitised to remove any sensitive data
 
