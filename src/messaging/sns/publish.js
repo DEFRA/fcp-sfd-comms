061f981710d334514f9cbad6a33e250eedb3d1b2
Simon Dunn <simondunn90@gmail.com>
2025-10-29 14:20:11 +0000

    Fix redacted data being sent in email contents (#36)

--- src/messaging/sns/publish.js
+++ src/messaging/sns/publish.js
@@ -8,6 +8,8 @@ const publish = async (snsClient, topicArn, message) => {
   }
 
   debugLog(message)
+  // this uses logger.debug which will only execute when cdp-app-config value LOG_LEVEL is set to debug
+  // any messages sent to the debugLog function are sanitised to remove any sensitive data
 
   const command = new PublishCommand(params)
 
