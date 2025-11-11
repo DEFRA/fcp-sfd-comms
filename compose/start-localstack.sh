c2181e6fbf985d241d3075fc6502ae5315d1644a
Simon Dunn <simondunn90@gmail.com>
2025-11-11 07:36:30 +0000

    reconfigure docker setup to use override file

--- compose/start-localstack.sh
+++ compose/start-localstack.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "creating localstack resources"
+echo "----------"
 aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name fcp_sfd_comms_request-deadletter
 aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name fcp_sfd_comms_request --attributes "{\"RedrivePolicy\": \"{\\\"deadLetterTargetArn\\\":\\\"arn:aws:sqs:eu-west-2:000000000000:fcp_sfd_comms_request-deadletter\\\",\\\"maxReceiveCount\\\":\\\"10\\\"}\"}"
 
